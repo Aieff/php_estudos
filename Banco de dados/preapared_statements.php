7d2bcc1a72884ca1cd9086d0f217b1822bd5a369
Gabriel Oliveira <gabrieloliveira311@outlook.com>
2023-11-19 21:47:58 -0300

    .

--- Banco de dados/preapared_statements.php
+++ Banco de dados/preapared_statements.php
@@ -15,3 +15,21 @@
     $stmt->bind_param("ss", $nome, $descricao); // s = string, i = integer, d = double
 
     $stmt->execute();
+
+
+
+
+    // Selecionando dados com prepared statements
+    $id = 4;
+
+    $stmt = $conn -> prepare("SELECT * FROM itens WHERE id > ?");
+
+    $stmt->bind_param("i", $id);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    $data = $result->fetch_all();
+
+    print_r($data);
